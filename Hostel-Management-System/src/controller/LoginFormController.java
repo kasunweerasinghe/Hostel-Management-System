442c59cefb97353ab6483cafbfef0d6e0112145a
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-06-29 07:38:38 +0530

    Added Login Form

---
+++ Hostel-Management-System/src/controller/LoginFormController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane LogInFormContext;
+    public JFXTextField txtUserName;
+    public JFXPasswordField pwdPassword;
+
+    public void btnLogInOnAction(ActionEvent actionEvent) {
+    }
+}
